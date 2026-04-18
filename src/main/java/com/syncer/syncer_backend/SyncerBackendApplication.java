123fc95aced3563b48eab73d0eb79112dba1c21e
Avi-0135 <aviralbarthwal981@gmail.com>
2026-04-18 22:13:34 +0530

    Phase 1 complete - Spring Boot project setup

---
+++ src/main/java/com/syncer/syncer_backend/SyncerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.syncer.syncer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syncer/syncer_backend/SyncerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syncer.syncer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
